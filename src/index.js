43d69ec1e8501e7096bcbef24ad3bd50fb7c97a9
AlexLapkin <130095964+AlexLapkin@users.noreply.github.com>
2024-04-23 22:17:53 +0300

    add payment-form for test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-import './css/styles.css';
+// import './css/styles.css';
+
+const paymentForm = document.querySelector('.payment-form');
+
+// Function-listener payment form
+async function onSubmit(event) {
+  let amount = document.getElementsByName('amount')[0].value;
+
+  fetch('/makePayment', {
+    method: 'POST',
+    body: JSON.stringify({ amount: amount }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+paymentForm.addEventListener('submit', onSubmit);
